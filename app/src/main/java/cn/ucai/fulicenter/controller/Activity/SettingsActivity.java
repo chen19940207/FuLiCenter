63757f8d1c426102ca9b86109e6eae93463fc120
chenqian <1173006527@qq.com>
2017-01-18 18:57:04 +0800

    上传头像

--- app/src/main/java/cn/ucai/fulicenter/controller/Activity/SettingsActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/Activity/SettingsActivity.java
@@ -1,11 +1,14 @@
 package cn.ucai.fulicenter.controller.Activity;
 
+import android.app.ProgressDialog;
+import android.app.backup.FullBackupDataOutput;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,10 +16,16 @@ import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.FuLiCenterApplication;
 import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.bean.Result;
 import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.net.ModelUser;
+import cn.ucai.fulicenter.model.net.OnCompleteListener;
 import cn.ucai.fulicenter.model.net.SharePrefrenceUtils;
+import cn.ucai.fulicenter.model.utils.CommonUtils;
 import cn.ucai.fulicenter.model.utils.ImageLoader;
 import cn.ucai.fulicenter.model.utils.L;
+import cn.ucai.fulicenter.model.utils.OnSetAvatarListener;
+import cn.ucai.fulicenter.model.utils.ResultUtils;
 import cn.ucai.fulicenter.view.DisplayUtils;
 import cn.ucai.fulicenter.view.MFGT;
 
@@ -29,6 +38,10 @@ public class SettingsActivity extends AppCompatActivity {
     TextView mtvUserProfileName;
     @BindView(R.id.tv_user_profile_nick)
     TextView mtvUserProfileNick;
+
+
+    OnSetAvatarListener mOnSetAvatarListener;
+    ModelUser model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,10 +85,65 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        L.e(TAG,"resultCode="+requestCode);
-        if (resultCode == RESULT_OK && requestCode == I.REQUEST_CODE_NICK) {
-            L.e(TAG,"resultCode="+requestCode);
-            mtvUserProfileNick.setText(FuLiCenterApplication.getUser().getMuserNick());
+        if (resultCode != RESULT_OK) {
+            return;
         }
+        if (requestCode == I.REQUEST_CODE_NICK) {
+            mtvUserProfileNick.setText(FuLiCenterApplication.getUser().getMuserNick());
+        } else if (requestCode == OnSetAvatarListener.REQUEST_CROP_PHOTO) {
+            uploadAvatar();
+        } else {
+            mOnSetAvatarListener.setAvatar(requestCode, data, mivUserProfileAvatar);
+        }
+    }
+
+    private void uploadAvatar() {
+        final User user = FuLiCenterApplication.getUser();
+        model = new ModelUser();
+        final ProgressDialog dilog = new ProgressDialog(this);
+        dilog.setMessage(getString(R.string.update_user_avatar));
+        dilog.show();
+        File file = null;
+        file = new File(String.valueOf(OnSetAvatarListener.getAvatarFile(this,
+                I.AVATAR_TYPE_USER_PATH+ "/" + user.getMuserName() + user.getMavatarSuffix())));
+        L.e(TAG, "file=" + file.getAbsolutePath());
+        model.updateAvatar(this,
+                user.getMuserName(),
+                file,
+                new OnCompleteListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        int msg = R.string.update_user_avatar_fail;
+                        if (s != null) {
+                            Result result = ResultUtils.getResultFromJson(s, User.class);
+                            if (result != null) {
+                                if (result.isRetMsg()) {
+                                    msg = R.string.update_user_avatar_success;
+                                }
+                            }
+                        }
+                        CommonUtils.showLongToast(msg);
+                        dilog.dismiss();
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        CommonUtils.showLongToast(error);
+                        dilog.dismiss();
+                    }
+                });
+    }
+
+    @OnClick(R.id.layout_user_profile_username)
+    public void onClickUserName() {
+        CommonUtils.showLongToast(R.string.username_connot_be_modify);
+    }
+
+    @OnClick(R.id.iv_user_profile_avatar)
+    public void onClickAvatar() {
+        mOnSetAvatarListener = new OnSetAvatarListener(this,
+                R.id.iv_user_profile_avatar,
+                FuLiCenterApplication.getUser().getMuserName(),
+                I.AVATAR_TYPE_USER_PATH);
     }
 }
--- app/src/main/java/cn/ucai/fulicenter/model/net/IModeUser.java
+++ app/src/main/java/cn/ucai/fulicenter/model/net/IModeUser.java
@@ -2,6 +2,8 @@ package cn.ucai.fulicenter.model.net;
 
 import android.content.Context;
 
+import java.io.File;
+
 import cn.ucai.fulicenter.model.bean.BoutiqueBean;
 import cn.ucai.fulicenter.model.utils.OkHttpUtils;
 
@@ -13,4 +15,5 @@ public interface IModeUser {
     void login(Context context,String username,String password, OnCompleteListener<String> listener);
     void register(Context context, String username, String nick, String password,OnCompleteListener<String> listener);
     void updateNick(Context context, String username, String userNick, OnCompleteListener<String> listener);
+    void updateAvatar(Context context, String username, File file, OnCompleteListener<String> listener);
 }
